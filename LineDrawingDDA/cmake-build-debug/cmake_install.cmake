03b4fc64d4591e1790e9e0671e96b980d8fa8e92
Mohamed Salah <mosala71352003@outlook.com>
2024-03-24 14:47:34 +0200

    Updated read me file

--- LineDrawingDDA/cmake-build-debug/cmake_install.cmake
+++ LineDrawingDDA/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/mosal/CLionProjects/LineDrawingDDA
+# Install script for directory: C:/Sala7/Github Projects/Graphics/Graphics/LineDrawingDDA
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/mosal/CLionProjects/LineDrawingDDA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Sala7/Github Projects/Graphics/Graphics/LineDrawingDDA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Graphics
+This repositary contains some of the algorithms I have larnt in the graphics course
